7b2364f13a06b99f6d400dc1202b424a1912661e
Molin, L <2473686l@student.gla.ac.uk>
2019-11-12 16:13:17 +0000

    Pass captcha check

--- fetchdata.py
+++ fetchdata.py
@@ -9,7 +9,7 @@
 # Copyright  © Rockface 2019 - 2020
 # --------------------------------------------------------------
 
-import request
+import requests
 import time
 import datetime
 import pytz
@@ -19,9 +19,32 @@ def Login(username, password):
     pass
 
 
-def needCaptcha(fduid):
-    # Generate time stamp for needcaptcha
+def needCaptcha(fduid, session):
+    '''
+    Generate time stamp for needcaptcha
+    Auth will be expired 1 day 
+    '''
+
+    header = {
+        "Host": "uis.fudan.edu.cn",
+        "Connection": "keep-alive",
+        "Accept": "text/plain, */*; q=0.01",
+        "DNT": "1",
+        "X-Requested-With": "XMLHttpRequest",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
+        "Referer": "http://uis.fudan.edu.cn/authserver/login?service=http%3A%2F%2Fjwfw.fudan.edu.cn%2Feams%2Flogin.action",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,ja;q=0.6"
+    }
     pst_now_0 = datetime.datetime.now(pytz.timezone("Etc/GMT+0"))
     time_stamp = int(float(pst_now_0.strftime("%s.%f"))*1000)
     url = "http://uis.fudan.edu.cn/authserver/needCaptcha.html?username=%s&_=%s" % (
         fduid, time_stamp)
+    response = session.get(url, headers=header)
+    return response
+
+
+if __name__ == "__main__":
+    s = requests.Session()
+    resp = needCaptcha("15301020060", s)
+    print(resp)
