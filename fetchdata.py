c0fef62dc33ec6a424ebbebc9d839e0cf6d5e3a2
Molin, L <2473686l@student.gla.ac.uk>
2019-11-12 15:56:12 +0000

    Time stamp

--- fetchdata.py
+++ fetchdata.py
@@ -8,3 +8,20 @@
 # Last Modified: Tuesday, 12th November 2019 2:36:58 pm
 # Copyright  © Rockface 2019 - 2020
 # --------------------------------------------------------------
+
+import request
+import time
+import datetime
+import pytz
+
+
+def Login(username, password):
+    pass
+
+
+def needCaptcha(fduid):
+    # Generate time stamp for needcaptcha
+    pst_now_0 = datetime.datetime.now(pytz.timezone("Etc/GMT+0"))
+    time_stamp = int(float(pst_now_0.strftime("%s.%f"))*1000)
+    url = "http://uis.fudan.edu.cn/authserver/needCaptcha.html?username=%s&_=%s" % (
+        fduid, time_stamp)
